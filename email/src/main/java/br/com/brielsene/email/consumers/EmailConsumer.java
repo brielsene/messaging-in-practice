26b80dd54d7684a47cc0aa4fdbe11e1155dc3b79
gabrielcosta <sene300@gmail.com>
2024-01-31 22:05:30 -0300

    adding funcional email service

--- email/src/main/java/br/com/brielsene/email/consumers/EmailConsumer.java
+++ email/src/main/java/br/com/brielsene/email/consumers/EmailConsumer.java
@@ -2,13 +2,31 @@ package br.com.brielsene.email.consumers;
 
 import br.com.brielsene.email.dtos.EmailRecordDto;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailConsumer {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String emailFrom;
+
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailRecordDto emailRecordDto){
         System.out.println(emailRecordDto.emailTo());
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom(emailFrom);
+        simpleMailMessage.setTo(emailFrom);
+        simpleMailMessage.setText(emailRecordDto.text());
+        simpleMailMessage.setSubject(emailRecordDto.subject());
+        javaMailSender.send(simpleMailMessage);
+
     }
 }
